0d822a9b8f10013accae44a343b6ad5480f6c8ce
mouadhltifi <mouadh.ltifi@hotmail.com>
2023-06-04 12:42:07 +0200

    seems working, issue with output

--- main.c
+++ main.c
@@ -2,165 +2,140 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_CAR_RANGE 1000000
+#define MAX_STATIONS 512
 
-typedef struct {
-    int range;
-} Car;
-
-typedef struct Node {
-    Car* car;
-    struct Node* next;
-} Node;
-
-typedef struct {
+typedef struct Station {
     int distance;
-    Node* cars;
+    int maxRange;
 } Station;
 
-typedef struct {
-    Station* stations;
-    int len;
-} Road;
+Station* stations[MAX_STATIONS];
+int numStations = 0;
 
-// Function prototypes.
-Road* createRoad(int len);
-Station* createStation(int distance);
-Car* createCar(int range);
-void addStation(Road* road, Station* station);
-void removeStation(Road* road, int distance);
-void addCar(Station* station, Car* car);
-void removeCar(Station* station, Car* car);
-void calculateShortestPath(Road* road, int start, int end);
-
-// Function implementations.
-Road* createRoad(int len) {
-    Road* road = malloc(sizeof(Road));
-    road->stations = malloc(sizeof(Station) * len);
-    road->len = len;
-    return road;
+Station* createStation(int distance, int maxRange) {
+    Station* newStation = (Station*)malloc(sizeof(Station));
+    newStation->distance = distance;
+    newStation->maxRange = maxRange;
+    return newStation;
 }
 
-Station* createStation(int distance) {
-    Station* station = malloc(sizeof(Station));
-    station->distance = distance;
-    station->cars = NULL;
-    return station;
-}
-
-Car* createCar(int range) {
-    Car* car = malloc(sizeof(Car));
-    car->range = range;
-    return car;
-}
-
-void addStation(Road* road, Station* station) {
-    road->stations[road->len++] = *station;
-}
-
-void removeStation(Road* road, int distance) {
-    // Implement this function.
-}
-
-void addCar(Station* station, Car* car) {
-    Node* node = malloc(sizeof(Node));
-    node->car = car;
-    node->next = station->cars;
-    station->cars = node;
-}
-
-void removeCar(Station* station, Car* car) {
-    // Implement this function.
-}
-
-void calculateShortestPath(Road* road, int start, int end) {
-    int* dp = malloc(sizeof(int) * (end + 1));
-    int* prev = malloc(sizeof(int) * (end + 1));
-    for (int i = 0; i <= end; ++i) {
-        dp[i] = MAX_CAR_RANGE;
-        prev[i] = -1;
-    }
-    dp[start] = 0;
-
-    for (int i = start + 1; i <= end; ++i) {
-        for (int j = 0; j < road->len; ++j) {
-            Station* station = &road->stations[j];
-            if (station->distance > i) break;
-            Node* node = station->cars;
-            while (node != NULL) {
-                if (station->distance + node->car->range >= i) {
-                    if (dp[i] > dp[station->distance] + 1) {
-                        dp[i] = dp[station->distance] + 1;
-                        prev[i] = station->distance;
-                    }
-                }
-                node = node->next;
-            }
+Station* findStation(int distance) {
+    for (int i = 0; i < numStations; i++) {
+        if (stations[i]->distance == distance) {
+            return stations[i];
         }
     }
+    return NULL;
+}
 
-    int current = end;
-    if (prev[current] == -1) {
-        printf("No path from %d km to %d km.\n", start, end);
-        free(prev);
-        free(dp);
+int addStation(int distance, int maxRange) {
+    if (findStation(distance) == NULL) {
+        stations[numStations++] = createStation(distance, maxRange);
+        return 1;
+    }
+    return 0;
+}
+
+void removeStation(int distance) {
+    for (int i = 0; i < numStations; i++) {
+        if (stations[i]->distance == distance) {
+            free(stations[i]);
+            stations[i] = stations[--numStations];
+            return;
+        }
+    }
+}
+
+void planRoute(int startDistance, int endDistance) {
+    Station* start = findStation(startDistance);
+    Station* end = findStation(endDistance);
+    if (start == NULL || end == NULL || start->maxRange == 0) {
+        printf("nessun percorso\n");
         return;
     }
-    printf("%d", current);
-    while (prev[current] != -1) {
-        printf(" <- %d", prev[current]);
-        current = prev[current];
-    }
-    printf("\n");
 
-    free(prev);
-    free(dp);
-}
+    int direction = (startDistance < endDistance) ? 1 : -1;
+    int route[MAX_STATIONS];
+    int routeLen = 0;
+    route[routeLen++] = startDistance;
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
-        printf("Usage: ./program <input_file>\n");
-        return 1;
-    }
-
-    FILE* file = fopen(argv[1], "r");
-    if (!file) {
-        printf("Could not open file: %s\n", argv[1]);
-        return 1;
-    }
-
-    Road* road = createRoad(10);
-    char command[20];
-    while (fscanf(file, "%s", command) != EOF) {
-        if (strcmp(command, "add") == 0) {
-            fscanf(file, "%s", command);
-            if (strcmp(command, "station") == 0) {
-                int distance;
-                fscanf(file, "%d", &distance);
-                addStation(road, createStation(distance));
-                printf("Added station at %d km.\n", distance);
-            } else if (strcmp(command, "car") == 0) {
-                int distance, range;
-                fscanf(file, "%d%d", &distance, &range);
-                for (int i = 0; i < road->len; ++i) {
-                    if (road->stations[i].distance == distance) {
-                        addCar(&road->stations[i], createCar(range));
-                        printf("Added car with range %d km to station at %d km.\n", range, distance);
-                        break;
-                    }
-                }
+    while (start->distance != endDistance) {
+        Station* next = NULL;
+        for (int i = 0; i < numStations; i++) {
+            if (direction == 1 && stations[i]->distance > start->distance && stations[i]->distance <= start->distance + start->maxRange) {
+                if (next == NULL || stations[i]->distance > next->distance)
+                    next = stations[i];
             }
-        } else if (strcmp(command, "remove") == 0) {
-            // Implement this part.
-        } else if (strcmp(command, "calculate") == 0) {
-            fscanf(file, "%s", command);
-            if (strcmp(command, "path") == 0) {
-                int start, end;
-                fscanf(file, "%d%d", &start, &end);
-                calculateShortestPath(road, start, end);
+            else if (direction == -1 && stations[i]->distance < start->distance && stations[i]->distance >= start->distance - start->maxRange) {
+                if (next == NULL || stations[i]->distance < next->distance)
+                    next = stations[i];
             }
         }
+        if (next == NULL) {
+            printf("nessun percorso\n");
+            return;
+        }
+        route[routeLen++] = next->distance;
+        start = next;
     }
-    fclose(file);
+    for (int i = 0; i < routeLen; i++) {
+        printf("%d ", route[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    char cmd[30];
+    int dist, numCars, carRange, ret;
+
+    while ((ret = scanf("%s", cmd)) != EOF) {
+        if(ret != 1) break;
+        if (strcmp(cmd, "aggiungi-stazione") == 0) {
+            int maxRange = 0;
+            if(scanf("%d %d", &dist, &numCars) != 2) break;
+            for (int i = 0; i < numCars; i++) {
+                if(scanf("%d", &carRange) != 1) break;
+                if (carRange > maxRange)
+                    maxRange = carRange;
+            }
+            if (addStation(dist, maxRange)) printf("aggiunta\n");
+            else printf("non aggiunta\n");
+        } else if (strcmp(cmd, "demolisci-stazione") == 0) {
+            if(scanf("%d", &dist) != 1) break;
+            Station* s = findStation(dist);
+            if (s) {
+                removeStation(dist);
+                printf("demolita\n");
+            } else {
+                printf("non demolita\n");
+            }
+        }else if (strcmp(cmd, "aggiungi-auto") == 0) {
+            if(scanf("%d %d", &dist, &carRange) != 2) break;
+            Station* s = findStation(dist);
+            if (s && carRange > s->maxRange) {
+                s->maxRange = carRange;
+                printf("aggiunta\n");
+            } else {
+                printf("non aggiunta\n");
+            }
+        } else if (strcmp(cmd, "rottama-auto") == 0) {
+            if(scanf("%d %d", &dist, &carRange) != 2) break;
+            Station* s = findStation(dist);
+            if (s && s->maxRange == carRange) {
+                s->maxRange = 0;
+                printf("rottamata\n");
+            } else {
+                printf("non rottamata\n");
+            }
+        } else if (strcmp(cmd, "pianifica-percorso") == 0) {
+            int start, end;
+            if(scanf("%d %d", &start, &end) != 2) break;
+            planRoute(start, end);
+        }
+    }
+
+    for (int i = 0; i < numStations; i++) {
+        free(stations[i]);
+    }
     return 0;
 }
